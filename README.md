17e33239c89dad2368400e619d964a7c719ff8c7
Bjoern Ruehle (e) <b_ruehle@DEMO0567.local>
2019-07-26 07:50:37 +0200

    frontend test

---
+++ README.md
@@ -0,0 +1,14 @@
+#Setup the project
+
+We assume that you have already npm and node installed.
+
+1. Install gulp globally on your machine 
+```
+sudo npm install -g gulp-cli
+```
+2. Open your terminal an switch to the project folder 
+3. Install necessary node modules 
+```
+npm install
+```
+4. Run 'gulp' with default task to open you local server. SCSS compiler and the reference to the CSS file is already done for you.
---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    sourcemaps = require("gulp-sourcemaps"),
+    browserSync = require("browser-sync").create();
+
+
+// Put this after including our dependencies
+var paths = {
+    styles: {
+        src: "assets/scss/**/*.scss",
+        dest: "assets/css"
+    }
+};
+     
+function style() {
+    return (
+        gulp
+            .src(paths.styles.src)
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .on("error", sass.logError)
+            .pipe(postcss([autoprefixer(), cssnano()]))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(paths.styles.dest))
+            .pipe(browserSync.stream())
+    );
+}
+    
+function reload() {
+    browserSync.reload();
+}
+ 
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch(paths.styles.src, style);
+    gulp.watch("*.html", reload);
+}
+
+
+exports.style = style;
+exports.watch = watch
+exports.default = watch
